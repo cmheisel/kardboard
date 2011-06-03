008616daf93c114f85a095200a043a86cd9526bc
Chris Heisel <chris@heisel.org>
2011-06-02 20:38:06 -0400

    Fixed bloody tabs

--- kardboard/__init__.py
+++ kardboard/__init__.py
@@ -8,8 +8,8 @@ __version__ = "0.1"
 app = Flask(__name__)
 app.config.from_object('kardboard.default_settings')
 if os.getenv('KARDBOARD_SETTINGS', None):
-	app.config.from_envvar('KARDBOARD_SETTINGS')
+    app.config.from_envvar('KARDBOARD_SETTINGS')
 app.db = MongoEngine(app)
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
