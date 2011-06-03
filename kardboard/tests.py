a49a58379a0ae8bdfe34cb97569568c97facc22a
Chris Heisel <chris@heisel.org>
2011-06-02 21:05:13 -0400

    Fixed tabs, added business_days_between and tests.

--- kardboard/tests.py
+++ kardboard/tests.py
@@ -1,40 +1,52 @@
 import datetime
 import unittest2
 
+
 class KardboardTestCase(unittest2.TestCase):
-	def setUp(self):
-		import kardboard
-		kardboard.app.config['MONGODB_DB'] = 'kardboard-unittest'
-		self.app = kardboard.app.test_client()
+    def setUp(self):
+        import kardboard
+        kardboard.app.config['MONGODB_DB'] = 'kardboard-unittest'
+        self.app = kardboard.app.test_client()
 
-	def tearDown(self):
-		from mongoengine.connection import _get_db
-		db = _get_db()
-		#Truncate/wipe the test database
-		[ db.drop_collection(name) for name in db.collection_names() \
-			if 'system.' not in name ]
+    def tearDown(self):
+        from mongoengine.connection import _get_db
+        db = _get_db()
+        #Truncate/wipe the test database
+        [ db.drop_collection(name) for name in db.collection_names() \
+            if 'system.' not in name ]
 
-	def _get_target_class(self):
-		raise NotImplementedError
+    def _get_target_class(self):
+        raise NotImplementedError
 
-	def _make_one(self, *args, **kwargs):
-		return self._get_target_class()(*args, **kwargs)
+    def _make_one(self, *args, **kwargs):
+        return self._get_target_class()(*args, **kwargs)
+
+
+class UtilTests(unittest2.TestCase):
+    def test_business_days(self):
+        from kardboard.util import business_days_between
+
+        wednesday = datetime.datetime(year=2011, month=6, day=1)
+        next_wednesday = datetime.datetime(year=2011, month=6, day=8)
+        result = business_days_between(wednesday, next_wednesday)
+        self.assertEqual(result, 5)
 
 
 class KardTests(KardboardTestCase):
-	def _get_target_class(self):
-		from kardboard.models import Kard
-		return Kard
+    def _get_target_class(self):
+        from kardboard.models import Kard
+        return Kard
 
-	def test_valid_card(self):
-		required_fields = {
-			'key': "CMSAD-1",
-			'title': "There's always money in the banana stand",
-			'backlog_date': datetime.datetime.now()
-		}
-		k = self._make_one(**required_fields)
-		k.save()
-		self.assert_(k.id)
+    def test_valid_card(self):
+        required_fields = {
+            'key': "CMSAD-1",
+            'title': "There's always money in the banana stand",
+            'backlog_date': datetime.datetime.now()
+        }
+        k = self._make_one(**required_fields)
+        k.save()
+        self.assert_(k.id)
+
 
 if __name__ == "__main__":
-	unittest2.main()
+    unittest2.main()
---
+++ kardboard/util.py
@@ -0,0 +1,15 @@
+import datetime
+
+def business_days_between(date1, date2):
+    if date1 < date2:
+        oldest_date, youngest_date = date1, date2
+    else:
+        oldest_date, youngest_date = date2, date1
+
+    business_days = 0
+    date = oldest_date
+    while date < youngest_date:
+        if date.weekday() != 5 and date.weekday() != 6:
+            business_days += 1
+        date = date + datetime.timedelta(days=1)
+    return business_days
