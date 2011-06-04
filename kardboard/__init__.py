93c3408b7de9f3e9f15025bb4906eabfa65ede81
Chris Heisel <chris@heisel.org>
2011-06-04 09:38:50 -0400

    Got local runserver working

--- kardboard/__init__.py
+++ kardboard/__init__.py
@@ -6,12 +6,10 @@ from flaskext.mongoengine import MongoEngine
 __version__ = "0.1"
 
 app = Flask(__name__)
+
 app.config.from_object('kardboard.default_settings')
 if os.getenv('KARDBOARD_SETTINGS', None):
     app.config.from_envvar('KARDBOARD_SETTINGS')
 app.db = MongoEngine(app)
 
 import kardboard.views
-
-if __name__ == "__main__":
-    app.run(debug=True)
---
+++ kardboard/runserver.py
@@ -0,0 +1,2 @@
+from kardboard import app
+app.run(debug=True)
