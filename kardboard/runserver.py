bdbe18e2a447e7fee1a112ee3c74f1b6dc3c789a
Chris Heisel <chris@heisel.org>
2011-06-23 07:16:35 -0400

    Added ability to run server in profiling mode.

--- kardboard/runserver.py
+++ kardboard/runserver.py
@@ -1,2 +1,22 @@
+import sys
 from kardboard import app
-app.run(debug=True)
+
+
+def profile_run():
+    from werkzeug import run_simple
+    from werkzeug.contrib.profiler import ProfilerMiddleware, MergeStream
+    print "* Profiling"
+    f = open('./profiler.log', 'w')
+    profiled_app = ProfilerMiddleware(app, MergeStream(sys.stderr, f))
+    run_simple('localhost', 5000, profiled_app,
+        use_reloader=True, use_debugger=True)
+
+
+def run():
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    if "profile" in sys.argv:
+        profile_run()
+    else:
+        run()
