b24ea0021dc57d55723fb680e8c116fd23b31ce7
Chris Heisel <chris@heisel.org>
2011-07-28 09:06:44 -0400

    Only preload data for cards that are missing it.

--- bin/preloadticket.py
+++ bin/preloadticket.py
@@ -2,4 +2,6 @@ from kardboard.models import Kard
 from kardboard.tasks import update_ticket
 
 for k in Kard.objects.all():
-    update_ticket.apply_async((k.id,))
+    if not k._ticket_system_data:
+        print "Scheduling update for %s" % k.key
+        update_ticket.apply_async((k.id,))
