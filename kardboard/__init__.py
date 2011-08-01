04f63541df422883b21fe02924d361a5c2bc853f
Chris Heisel <chris@heisel.org>
2011-08-01 15:40:54 -0400

    Allowing for configurable log level and log file location

--- kardboard/__init__.py
+++ kardboard/__init__.py
@@ -5,11 +5,17 @@ from flaskext.mongoengine import MongoEngine
 from flaskext.cache import Cache
 from flaskext.celery import Celery
 import mongoengine
+import logging
 
 __version__ = "1.3.3"
 
 app = Flask(__name__)
 
+LEVELS = {'debug': logging.DEBUG,
+          'info': logging.INFO,
+          'warning': logging.WARNING,
+          'error': logging.ERROR,
+          'critical': logging.CRITICAL}
 
 class PortAwareMongoEngine(MongoEngine):
     def init_app(self, app):
@@ -26,6 +32,28 @@ class PortAwareMongoEngine(MongoEngine):
 app.config.from_object('kardboard.default_settings')
 if os.getenv('KARDBOARD_SETTINGS', None):
     app.config.from_envvar('KARDBOARD_SETTINGS')
+
+if app.config.get('LOG_FILE'):
+    log_file = app.config['LOG_FILE']
+    log_file = os.path.abspath(os.path.expanduser(log_file))
+    new_handler = logging.handlers.RotatingFileHandler(
+        log_file, maxBytes=1000, backupCount=3)
+    if app.config.get('LOG_LEVEL'):
+        new_level = app.config['LOG_LEVEL']
+        new_level = LEVELS.get(new_level, logging.error)
+        new_handler.setLevel(new_level)
+
+    log_format = (
+        '-' * 80 + '\n' +
+        '%(asctime)-15s\n%(levelname)s in %(module)s [%(pathname)s:%(lineno)d]:\n' +
+        '%(message)s\n' +
+        '-' * 80
+    )
+    new_handler.setFormatter(logging.Formatter(log_format))
+
+    app.logger.addHandler(new_handler)
+
+
 app.secret_key = app.config['SECRET_KEY']
 app.db = PortAwareMongoEngine(app)
 
