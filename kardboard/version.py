3c32e8b653918d0aff126f4587a8a0ec1060307b
Chris Heisel <chris@heisel.org>
2011-08-12 18:04:44 -0400

    Another attempt to fix #50

--- kardboard/version.py
+++ kardboard/version.py
@@ -3,7 +3,7 @@ import os
 from kardboard.util import get_git_version
 
 this_folder = os.path.dirname(os.path.abspath(__file__))
-version_file = os.path.join(this_folder, 'VERSION')
+version_file = os.path.join(this_folder, 'VERSION.txt')
 version = file(version_file, 'r').read()
 
 __git_version__ = get_git_version()
