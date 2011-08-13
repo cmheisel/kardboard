ec36839bdb2a2468fdc14666000bdf4ad40a1392
Chris Heisel <chris@heisel.org>
2011-08-13 13:55:56 -0400

    Fixed manage.py celeryd

--- kardboard/manage.py
+++ kardboard/manage.py
@@ -2,10 +2,12 @@
 
 from __future__ import absolute_import
 from flaskext.script import Manager
+from flaskext.celery import Celery
 from flaskext.celery import install_commands as install_celery_commands
 
 from kardboard.app import app
 
+celery = Celery(app)
 manager = Manager(app)
 install_celery_commands(manager)
 
