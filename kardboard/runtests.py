a255db2417ef2373893813c0c7bbc1a1e0125c19
Chris Heisel <chris@heisel.org>
2011-08-14 08:34:39 -0400

    runtests.py especially for continuous.io

---
+++ kardboard/runtests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# For use by continuous.io
+
+import unittest_continuous
+unittest_continuous.setup()
+
+from kardboard.tests import *
+
+import unittest2
+
+unittest2.main()
