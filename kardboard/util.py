7155a5dc26d7fd99fec177536d9aaae5385f88ae
Chris Heisel <chris@heisel.org>
2011-08-15 16:35:48 -0400

    Adds ticket summary to ticket detail page. Closes #39

--- kardboard/util.py
+++ kardboard/util.py
@@ -5,6 +5,9 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 
+import jinja2.ext
+import markdown2
+
 from flaskext.mongoengine import MongoEngine
 from werkzeug import import_string, cached_property
 import mongoengine
@@ -211,3 +214,31 @@ def configure_logging(app):
         new_handler.setFormatter(logging.Formatter(log_format))
 
         app.logger.addHandler(new_handler)
+
+
+
+
+class Markdown2Extension(jinja2.ext.Extension):
+    tags = set(['markdown2'])
+
+    def __init__(self, environment):
+        super(Markdown2Extension, self).__init__(environment)
+        environment.extend(
+            markdowner=markdown2.Markdown()
+        )
+
+    def parse(self, parser):
+        lineno = parser.stream.next().lineno
+        body = parser.parse_statements(
+            ['name:endmarkdown2'],
+            drop_needle=True
+        )
+        return jinja2.nodes.CallBlock(
+            self.call_method('_markdown_support'),
+            [],
+            [],
+            body
+        ).set_lineno(lineno)
+
+    def _markdown_support(self, caller):
+        return self.environment.markdowner.convert(caller()).strip()
