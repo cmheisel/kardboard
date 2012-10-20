0153428e8947a40c7c663f9497e50317d368c66b
Chris Heisel <chris@heisel.org>
2012-10-20 15:10:25 -0400

    Made move class migration idempotent

--- bin/m07-moveclass.py
+++ bin/m07-moveclass.py
@@ -2,6 +2,7 @@ from kardboard.models import Kard
 
 for k in Kard.objects.all().only('_type', '_service_class'):
     if k._type is None:
-        k._type = k._service_class
-        k._service_class = None
-        k.save()
+        if k._service_class is not None:
+            k._type = k._service_class
+            k._service_class = None
+            k.update(set___type=k._type, set___service_class=k._service_class)
