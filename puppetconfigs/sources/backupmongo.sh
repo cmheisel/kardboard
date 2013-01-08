c2fee4585f49f033395ed1c88a0dbddfaa0118a5
Chris Heisel <chris@heisel.org>
2013-01-08 08:38:07 -0500

    Removing old puppet configs

--- puppetconfigs/sources/backupmongo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-data_path=/home/kardboard/var/
-mongodump=/usr/bin/mongodump
-
-cd $data_path
-$mongodump
--- puppetconfigs/sources/mongologrotate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# See http://www.mongodb.org/display/DOCS/Logging
-supervisorctl stop all
-killall -SIGUSR1 mongod
-supervisorctl start all
