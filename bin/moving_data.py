97c485cb46c118bddc4a9cfce3310a3aa3852815
Chris Heisel <chris@heisel.org>
2013-01-21 17:12:53 -0500

    Fixed start/end date math on moving_data script

--- bin/moving_data.py
+++ bin/moving_data.py
@@ -1,13 +1,14 @@
 from dateutil.relativedelta import relativedelta
 from kardboard.models import Kard, ReportGroup, FlowReport, DailyRecord
-from kardboard.util import make_end_date
+from kardboard.util import make_start_date, make_end_date
 
 def parse_date(datestr):
     from dateutil import parser
     return parser.parse(datestr)
 
 def daily_throughput_average(stop, weeks=4):
-    start = stop - relativedelta(days=weeks * 7)
+    start = (stop - relativedelta(days=weeks * 7)) + relativedelta(days=1)
+    start = make_start_date(date=start)
 
     query = Kard.objects.filter(
         done_date__gte=start,
@@ -22,7 +23,7 @@ def find_wip(stop):
         group = 'dev',
     )
 
-    exclude = [u'Backlog', u'Done',]
+    exclude = [u'Backlog', u'Done', ]
     wip = 0
     for state, count in sl.state_counts.items():
         if state not in exclude:
@@ -59,23 +60,28 @@ def moving_data(start, stop):
         'features': len(features),
         'bugfixes': len(defects),
         'little_law': round(little_law),
-        'cycle_time_ave': round(cycle_time_ave)
+        'cycle_time_ave': round(cycle_time_ave),
+        'wip': wip,
     }
     return data
 
 def print_data(data):
-    print "%s -- %s: %s\t%s\t%s\t%s" % (
+    print "%s - %s\nF: %s\tB: %s\tL: %s\tC: %s\tW: %s" % (
         data['start'],
         data['stop'],
         data['features'],
         data['bugfixes'],
         data['little_law'],
         data['cycle_time_ave'],
+        data['wip'],
     )
 
 def weekly_moving_data(start):
     start = parse_date(start)
-    stop = start + relativedelta(days=7)
+    stop = start + relativedelta(days=6)
+
+    start = make_start_date(date=start)
+    stop = make_end_date(date=stop)
     return moving_data(start, stop)
 
 if __name__ == "__main__":
