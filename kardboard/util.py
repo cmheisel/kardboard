5f661d203c243b5b2c60050ecdafcf1e2de3e468
Chris Heisel <chris@heisel.org>
2013-01-22 19:19:08 -0500

    Fixed Travis M's favorite slugify pet peeve

--- kardboard/util.py
+++ kardboard/util.py
@@ -188,6 +188,7 @@ _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 def slugify(text, delim=u'-'):
     """Generates an ASCII-only slug."""
     result = []
+    text = text.replace("'", "")
     for word in _punct_re.split(text.lower()):
         word = word.encode('translit/long')
         if word:
