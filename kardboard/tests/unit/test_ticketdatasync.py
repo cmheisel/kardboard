1c39cb14eeffdb620d2b534b1d5123ea8507925c
Chris Heisel <chris@heisel.org>
2013-01-31 07:40:18 -0500

    Modified data sync tests to use unittest2 and mocks

--- kardboard/tests/unit/test_ticketdatasync.py
+++ kardboard/tests/unit/test_ticketdatasync.py
@@ -1,31 +1,28 @@
 from datetime import datetime
 
-from kardboard.tests.core import KardboardTestCase
+import unittest2
+
+from kardboard.tests.mocks import Mock
 
 
-class TicketDataSyncTests(KardboardTestCase):
+class TicketDataSyncTests(unittest2.TestCase):
     def setUp(self):
         super(TicketDataSyncTests, self).setUp()
         from kardboard.services import ticketdatasync
         self.service = ticketdatasync
 
     def test_due_date_changes_from_some_to_none(self):
-        k = self.make_card()
-        t = k.ticket_system_data
-        t['due_date'] = datetime.now()
+        kard = Mock()
+        kard.due_date = datetime.now()
 
-        self.service.set_due_date_from_ticket(k, t)
-
-        self.assert_(k.due_date)
-
-        t['due_date'] = None
-        self.service.set_due_date_from_ticket(k, t)
-        self.assertEqual(None, k.due_date)
+        ticket_data = {'due_date': None}
+        self.service.set_due_date_from_ticket(kard, ticket_data)
+        self.assertEqual(None, kard.due_date)
 
     def test_due_date_gets_set(self):
-        k = self.make_card()
-        t = k.ticket_system_data
-        t['due_date'] = datetime.now()
+        kard = Mock()
+        ticket_data = {}
+        ticket_data['due_date'] = datetime.now()
 
-        self.service.set_due_date_from_ticket(k, t)
-        self.assertEqual(t['due_date'], k.due_date)
+        self.service.set_due_date_from_ticket(kard, ticket_data)
+        self.assertEqual(ticket_data['due_date'], kard.due_date)
