6bf74ad80953e68012c29674dfe0d5a5e090e2b9
Chris Heisel <chris@heisel.org>
2013-02-10 14:42:39 -0500

    Drag and drop ajax call hooked up

--- kardboard/static/sortables.js
+++ kardboard/static/sortables.js
@@ -2,7 +2,7 @@ $(function() {
     $( "td.cards_backlog" ).sortable({
         items: "div.card_on_board:not(.ui-state-disabled)",
         cursor: "move",
-        revert: true
+        revert: true,
     });
 
     $( "td.cards_backlog div.card_on_board" ).disableSelection();
