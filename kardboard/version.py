a6bdd3d5ab4fd1b82d44a2bd061950c7182019a2
Chris Heisel <chris@heisel.org>
2013-04-22 14:21:49 -0400

    Remove git version cruft

--- kardboard/version.py
+++ kardboard/version.py
@@ -1,25 +1,6 @@
 import os
-import subprocess
 
 version = "1.9.3"
-
-
-def get_git_version():
-    p = subprocess.Popen(['which git'], shell=True, stdout=subprocess.PIPE)
-    returncode = os.wait()
-    has_git = returncode[-1] if returncode else -1
-    if has_git == 0:
-        location = os.path.dirname(os.path.abspath(__file__))
-        p = subprocess.Popen(
-            ['cd %s && git describe --tags' % location],
-            stdout=subprocess.PIPE, shell=True)
-        result = p.communicate()[0]
-        return '-%s' % result
-    else:
-        return ''
-
 this_folder = os.path.dirname(os.path.abspath(__file__))
-
-__git_version__ = get_git_version()
 __version__ = version
-VERSION = "%s%s" % (version, __git_version__)
+VERSION = version
