28d57b064396f4fb821626e15d9946a1daacf443
Chris Heisel <chris@heisel.org>
2013-04-22 11:50:08 -0400

    Logic fix to statelog script

--- bin/fix_unexited_statelogs.py
+++ bin/fix_unexited_statelogs.py
@@ -1,14 +1,24 @@
 from kardboard.models.kard import Kard
 from kardboard.models.statelog import StateLog
 
-bad_logs = StateLog.objects.filter(exited__exists=False, state__nin=["Done", ])
-for bad_log in bad_logs:
-    k = Kard.objects.get(key=bad_log.card.key)
+unexited_logs = StateLog.objects.filter(exited__exists=False, state__nin=["Done", ])
+
+fix_count = 0
+for unexited_log in unexited_logs:
+    try:
+        k = Kard.objects.get(key=unexited_log.card.key)
+    except AttributeError:
+        print "MISSING CARD"
+        pass
     logs = StateLog.objects.filter(card=k)
     for l in logs:
         if l.exited is None:
             for log in StateLog.objects.filter(card=k):
-                #print "%s %s %s %s" % (log.card.key, log.state, log.entered, log.exited)
                 if log.state == l.state and log.entered > l.entered:
+                    #print "%s" % l.card.key
+                    fix_count +=1
                     l.exited = log.entered
                     l.save()
+
+
+print "Fixed: %s" % (fix_count)
