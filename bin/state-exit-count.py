4a5482e66d78a4f0bb1d0898a8c624427485627d
Chris Heisel <chris@heisel.org>
2013-05-25 18:22:18 -0400

    #147 PEP8-ery

--- bin/state-exit-count.py
+++ bin/state-exit-count.py
@@ -6,11 +6,13 @@ from dateutil.relativedelta import relativedelta
 from kardboard.util import make_start_date, make_end_date, average, median
 from kardboard.models.statelog import StateLog
 
+
 def histogram(counts):
     d = defaultdict(int)
     for c in counts:
         d[c] += 1
     return dict(d)
+
 
 def exit_counts(state, months, raw=False):
     end = make_end_date(date=datetime.datetime.now())
@@ -21,14 +23,12 @@ def exit_counts(state, months, raw=False):
     counts = []
     data = []
 
-
     current_date = start
     while current_date <= end:
         if current_date.weekday() == 5:  # Saturday
             current_date += relativedelta(days=2)
         elif current_date.weekday() == 6:  # Sunday
             current_date += relativedelta(days=1)
-
 
         range_start = make_start_date(date=current_date)
         range_end = make_end_date(date=current_date)
@@ -61,7 +61,7 @@ def exit_counts(state, months, raw=False):
         for date, count in data:
             print "%s\t%s" % (date, count)
 
+
 if __name__ == "__main__":
     import sys
     exit_counts(sys.argv[1], int(sys.argv[2]))
-
