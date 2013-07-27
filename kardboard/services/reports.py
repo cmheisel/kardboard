29bee3848a5c7aa28bdafdb0ab1ccff40f991b6a
Chris Heisel <chris@heisel.org>
2013-07-27 09:55:06 -0400

    Added helper methods to CDR class for chart generation

--- kardboard/services/reports.py
+++ kardboard/services/reports.py
@@ -33,3 +33,40 @@ class CycleTimeDistribution(object):
             )
             rows.append(row)
         return rows
+
+    def days(self):
+        days = list(set([c.cycle_time for c in self.cards]))
+        days.sort()
+        return days
+
+    def service_classes(self):
+        sclasses = list(set([c.service_class['name'] for c in self.cards]))
+        sclasses.sort()
+        return sclasses
+
+    def _cards_by_service_class(self):
+        cards = {}
+        for c in self.cards:
+            sclass_cards = cards.get(c.service_class['name'], [])
+            sclass_cards.append(c)
+            cards[c.service_class['name']] = sclass_cards
+        return cards
+
+    def service_class_series(self):
+        days = self.days()
+
+        series = {}
+        for sclass, cards in self._cards_by_service_class().items():
+            seri = defaultdict(int)
+            for day in days:
+                seri[day] = 0
+            for c in cards:
+                seri[c.cycle_time] += 1
+
+            keys = seri.keys()
+            keys.sort()
+            values = []
+            for k in keys:
+                values.append(seri[k])
+            series[sclass] = values
+        return series
