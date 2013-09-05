b6e91da29651debee847934b1c93922f1906137c
Chris Heisel <chris@heisel.org>
2013-09-05 19:14:26 -0400

    Modified moving_data script to accept report group

--- bin/moving_data.py
+++ bin/moving_data.py
@@ -48,17 +48,17 @@ def find_cycle_time_ave(stop):
     return dr.moving_cycle_time
 
 
-def moving_data(start, stop):
+def moving_data(report_group_slug, start, stop):
     query = Kard.objects.filter(
         done_date__gte=start,
         done_date__lte=stop,)
 
-    kards = list(ReportGroup('dev', query).queryset)
+    kards = list(ReportGroup(report_group_slug, query).queryset)
     features = [k for k in kards if k.is_card]
     defects = [k for k in kards if not k.is_card]
     over_sla = [k for k in kards if k.cycle_time > k.service_class['upper']]
-    feature_cycle_ave = average([k.cycle_time for k in features])
-    feature_stddev = standard_deviation([k.cycle_time for k in features])
+    card_cycle_ave = average([k.cycle_time for k in kards]) or 0
+    card_stddev = standard_deviation([k.cycle_time for k in kards]) or 0
 
     wip = find_wip(stop)
     tpa = daily_throughput_average(stop)
@@ -74,8 +74,8 @@ def moving_data(start, stop):
         'little_law': int(round(little_law)),
         'cycle_time_ave': int(round(cycle_time_ave)),
         'wip': wip,
-        'feature_cycle_average': int(round(feature_cycle_ave)),
-        'feature_stddev': int(round(feature_stddev)),
+        'cycle_average': int(round(card_cycle_ave)),
+        'stddev': int(round(card_stddev)),
         'over_sla': len(over_sla),
     }
     return data
@@ -89,20 +89,24 @@ def print_data(data):
     print "Cycl: %s" % data['cycle_time_ave']
     print " WIP: %s" % data['wip']
     print "oSLA: %s" % data['over_sla']
-    print "fCyc: %s" % data['feature_cycle_average']
-    print "fStd: %s" % data['feature_stddev']
+    print "fCyc: %s" % data['cycle_average']
+    print "fStd: %s" % data['stddev']
 
 
-def weekly_moving_data(start):
+def weekly_moving_data(report_group_slug, start):
     start = parse_date(start)
     stop = start + relativedelta(days=6)
 
     start = make_start_date(date=start)
     stop = make_end_date(date=stop)
-    return moving_data(start, stop)
+    return moving_data(report_group_slug, start, stop)
 
 
 if __name__ == "__main__":
     import sys
-    data = weekly_moving_data(sys.argv[1])
+    try:
+        data = weekly_moving_data(sys.argv[1], sys.argv[2])
+    except IndexError:
+        print "{report_group_slug} {week_start_date}"
+        raise
     print_data(data)
